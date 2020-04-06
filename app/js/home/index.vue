4693bb19f9f412a6dc13b050db52c709460246f6
dongcaomaoco <dong@caomao.co>
2020-04-06 20:08:09 +0800

    添加其它文件到github.

---
+++ app/js/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div :class="$style.title">title</div>
+    <br/>
+    <div :class="$style.content">content</div>
+    <hr/>
+    <h1>
+      h1
+      <div :class="$style.subh1">
+        sub-h1
+      </div>
+    </h1>
+  </div>
+</template>
+
+<style lang="scss" module>
+  @import "../../css/reset.scss";
+  .title {
+    font-size:80px;
+    color:blue;
+  }
+</style>
+
+<script>
+  export default{
+
+  }
+</script>
